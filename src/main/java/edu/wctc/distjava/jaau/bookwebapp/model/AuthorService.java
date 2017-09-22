cba1e699a7af4a07b984fddb9ea0eb7ff055a98a
Aruni <Aruni@10.30.12.168>
2017-09-21 20:19:45 -0500

    second commit.

---
+++ src/main/java/edu/wctc/distjava/jaau/bookwebapp/model/AuthorService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wctc.distjava.jaau.bookwebapp.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+
+
+/**
+ *
+ * @author Aruni
+ */
+public class AuthorService {
+    
+    public List<Author> getAuthorList(){
+        return Arrays.asList(
+                new Author(1,"Mark Twain", new Date()),
+                new Author(2,"Stephen King", new Date()),
+                new Author(3,"George Orwell", new Date())
+        );
+        
+        
+    }
+}
